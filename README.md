6d01ccad0d16aac8a54ac3fe4e44b74613864161
Matthias De Gisi <matthias.degisi@gmail.com>
2024-12-04 16:46:32 -0800

    indent

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 - stow
 - tmux
 - nvim
+
+# Use
+- Download requirements
+- Clone the git repo (I like the gh cli)
+- Use stow *foldername* to link the config files in the right place
---
+++ nvim/.config/nvim/lua/plugins/indent.lua
@@ -0,0 +1,10 @@
+return {
+        {
+                'echasnovski/mini.indentscope',
+                version = false,
+                lazy = false,
+                config = function()
+                        require('mini.indentscope').setup()
+                end
+        }
+}
