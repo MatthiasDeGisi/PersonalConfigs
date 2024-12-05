f00e8df88844cb07491c523d31310ef6ff2d77a8
Matthias De Gisi <matthias.degisi@gmail.com>
2024-12-05 15:33:39 -0800

    tab spaces

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,3 +3,9 @@ require("config.lazy")
 
 -- To stop nvim from having mouse enabled (helps with moba copy paste)
 vim.opt.mouse = ""
+
+-- To tab 4 space
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
